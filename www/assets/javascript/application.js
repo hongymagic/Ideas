116370761c920b8c74b31c7a4e41a1a16b084cb4
David Hong <davidhong.code@gmail.com>
2012-02-15 10:26:30 +1100

    Simple Facebook login/logout mechanism implemented

--- www/assets/javascript/application.js
+++ www/assets/javascript/application.js
@@ -1,16 +1,71 @@
+if (typeof PhoneGap == 'undefined') alert('PhoneGap variable does not exist. Check that you have included phonegap.js correctly');
+if (typeof PG == 'undefined') alert('PG variable does not exist. Check that you have included pg-plugin-fb-connect.js correctly');
+if (typeof FB == 'undefined') alert('FB variable does not exist. Check that you have included the Facebook JS SDK file.');
+
+
 // Fired when application starts
 var documentReady = function () {
-	document.addEventListener("deviceready", onDeviceReady, false);
+  document.addEventListener("deviceready", onDeviceReady, false);
+};
+
+var c = {};
+c.log = function (message) {
+	navigator.notification.alert(message);
 };
 
 var onDeviceReady = function () {
-    $('[title]').tooltip();
-    $('.collapse')
-		.collapse({ toggle: false })
-		.on('shown', function () {
-			$('[data-target="#' + this.id + '"]').html('<i>☟</i> Hide comments');
-		})
-		.on('hidden', function () {
-			$('[data-target="#' + this.id + '"]').html('<i>☛</i> Show comments');
-		});
+	c.log('Ready');
+
+  $('[title]').tooltip();
+  $('.collapse').collapse({
+    toggle: false
+  }).on('shown', function () {
+    $('[data-target="#' + this.id + '"]').html('<i>☟</i> Hide comments');
+  }).on('hidden', function () {
+    $('[data-target="#' + this.id + '"]').html('<i>☛</i> Show comments');
+  });
+
+  // FB Integration testing
+  FB.init({ appId: "244777332265381", nativeInterface: PG.FB });
+
+  var updateLoginStatus = function () {
+	  FB.getLoginStatus(function(response) {
+	  	var $fb = $('#fb-login');
+
+	    if (response.status == 'connected') {
+	    	$fb
+	      	.text('Logout from Facebook')
+	      	.toggleClass('btn-warning')
+	      	.attr('data-role', 'fb:logout');
+	    } else {
+	    	$fb
+	      	.text('Login using Facebook')
+	      	.removeClass('btn-warning')
+	      	.attr('data-role', 'fb:login');
+	    }
+	  });
+	};
+
+	updateLoginStatus();
+
+  //
+  // Facebook login
+  $(document)
+	  .on('click', '[data-role=fb:login]', function (event) {
+		  FB.login(function (response) {
+		  	updateLoginStatus();
+
+	      if (!response.session) {
+	        alert('User cancelled login or did not fully authorize.');
+	      }
+	    }, {
+	      perms: 'email'
+	    });
+	  })
+	  .on('click', '[data-role="fb:logout"]', function (event) {
+	  	console.log('Logout');
+      FB.logout(function (response) {
+        updateLoginStatus();
+      });
+	  });
 };
